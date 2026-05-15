078575c3821d9163dcb9d48894e9ea3fce17d24f
Kaleo <kaleo.abreu.2009@gmail.com>
2026-05-15 15:48:48 -0300

    novas funções javascript

---
+++ Java Script/veiculos.js
@@ -0,0 +1,21 @@
+function mostrarPromocao() {
+    let inVeiculo = document.getElementById("inVeiculo");
+    let inPreco = document.getElementById("inPreco");
+
+    let outVeiculo = document.getElementById("outVeiculo");
+    let outEntrada = document.getElementById("outEntrada");
+    let outParcela = document.getElementById("outParcela");
+
+    let veiculo = inVeiculo.value;
+    let preco = Number(inPreco.value);
+
+    let entrada = preco * 0.50;
+    let parcela = entrada / 12;
+
+    outVeiculo.textContent = "Promoção: " + veiculo;
+    outEntrada.textContent = "Entrada de R$: " + entrada.toFixed(2);
+    outParcela.textContent = "+ 12x de R$: " + parcela.toFixed(2);
+}
+
+let btVerPromocao = document.getElementById("btVerPromocao");
+btVerPromocao.addEventListener("click", mostrarPromocao);
